63085951cc5b4a3c8bec3ceda856a1f48c955d69
Yamille <yamillepprnascimento@gmail.com>
2024-10-03 20:34:42 -0300

    Aplicando JS

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+
+//passo 1 -> pegar os personagens no JS pra alterar quando passar o mouse em cima
+
 const personagens = document.querySelectorAll(".personagem")
 
-personagens
+//passo 2 -> adicionar a classe "selecionado" no personagem que o usuário passar o mouse
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        
+
+//passo 3 -> verificar se já existe um personagem selecionado, se sim, devemos remover a seleção dele
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado');
+        personagem.classList.add('selecionado');
+    })
+
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+    const idPersonagem = personagem.attributes.id.value;
+
+    imagemPersonagemGrande.src = ""
+})
